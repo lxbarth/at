de67a824e2d0fe98d190263851224944305afb52
lxbarth <alex@mapbox.com>
2014-02-21 08:32:01 -0500

    Get to something halfway launch ready.

--- site.js
+++ site.js
@@ -1,29 +1,57 @@
 (function() {
-    var query = {};
-    (location.search.split('?')[1] || '')
-        .split('&')
-        .forEach(function(q) {
-            q = q.split('=');
-            if (q[0]) query[q[0]] = q[1] || true;
-        });
+    var parseHash = function() {
+        var hash = {};
+        (location.hash.split('#')[1] || '')
+            .split('&')
+            .forEach(function(q) {
+                q = q.split('=');
+                if (q[0]) hash[q[0]] = decodeURI(q[1]) || true;
+            });
+        return hash;
+    };
+    var setHash = function(params) {
+        var hash = parseHash(location.search);
+        for (var k in params) {
+            hash[k] = params[k];
+        }
+        var newHash = [];
+        for (var k in hash) {
+            newHash.push(k + '=' + encodeURI(hash[k]));
+        }
+        location.hash = newHash.join('&');
+    };
 
     var map = L.mapbox.map('map', 'lxbarth.map-lxoorpwz');
 
-    if (query.marker) {
+    var hash = parseHash();
+    if (hash.marker) {
         var icon = L.icon({
             iconUrl: 'http://api.tiles.mapbox.com/v3/marker/pin-l-circle-stroked+3C4E59.png',
             iconRetinaUrl: 'my-icon@2x.png',
             iconAnchor: [18, 45]
         });
-        var marker = query.marker.split('/');
+        var marker = hash.marker.split('/');
+        console.log(marker);
         L.marker(marker.slice(0, 2), {icon: icon}).addTo(map);
         map.setView(marker.slice(0, 2), marker[2]);
     }
-    if (query.text) {
-        var text = document.getElementById('text');
-        text.innerHTML = decodeURI(query.text);
+    var startMessage = "Type your message here";
+    var text = document.getElementById('text');
+    if (hash.text) {
+        text.innerHTML = hash.text;
         text.onclick = function(){
             marker && map.setView(marker.slice(0, 2), marker[2]);
+        };
+    } else {
+        text.setAttribute('contentEditable', true);
+        text.innerHTML = startMessage;
+        text.onclick = function() {
+            if (text.innerHTML == startMessage) {
+                text.innerHTML = '';
+            }
+        };
+        text.onkeypress = function() {
+            setHash({text: text.innerHTML});
         };
     }
 })();
