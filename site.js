338c8b9191cbffac35a9880d7009cd18f3cf56a2
lxbarth <alex@mapbox.com>
2014-02-20 21:19:13 -0500

    Add marker.

---
+++ site.js
@@ -0,0 +1,17 @@
+(function() {
+    var query = {};
+    (location.search.split('?')[1] || '')
+        .split('&')
+        .forEach(function(q) {
+            q = q.split('=');
+            if (q[0]) query[q[0]] = q[1] || true;
+        });
+
+    var map = L.mapbox.map('map', 'lxbarth.map-lxoorpwz');
+
+    if (query.marker) {
+        var marker = query.marker.split('/');
+        L.marker(marker.slice(0, 2)).addTo(map);
+        map.setView(marker.slice(0, 2), marker[2]);
+    }
+})();
