37df9fe2148c15768d59ef39aafe05fd48928434
lxbarth <alex@mapbox.com>
2014-02-20 22:18:04 -0500

    Add marker.

--- site.js
+++ site.js
@@ -10,8 +10,12 @@
     var map = L.mapbox.map('map', 'lxbarth.map-lxoorpwz');
 
     if (query.marker) {
+        var icon = L.icon({
+            iconUrl: 'http://api.tiles.mapbox.com/v3/marker/pin-l-star+3C4E59.png',
+            iconRetinaUrl: 'my-icon@2x.png'
+        });
         var marker = query.marker.split('/');
-        L.marker(marker.slice(0, 2)).addTo(map);
+        L.marker(marker.slice(0, 2), {icon: icon}).addTo(map);
         map.setView(marker.slice(0, 2), marker[2]);
     }
     if (query.text) {
